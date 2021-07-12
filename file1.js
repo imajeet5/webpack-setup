bd9cc5879476dff520d13ec42f21d9c2ec30df61
Ajeet Singh <imajeet5@gmail.com>
2021-07-12 15:19:20 +0530

    webpack initial setup

---
+++ file1.js
@@ -0,0 +1,12 @@
+class Animal {
+    constructor(name){
+        this.name = name;
+        
+    }
+    printName(){
+        console.log(this.name);
+    }
+    
+}
+
+export default Animal;
---
+++ main.js
@@ -0,0 +1,7 @@
+import Animal from './file1.js';
+import './styles.scss'
+
+const ani = new Animal("Abc123");
+ani.printName();
+
+console.log('Hi there');
